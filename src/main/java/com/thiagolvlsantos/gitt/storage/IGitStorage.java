fd5cb2adcfa41e701bc33b3e1cb3ed4172b25dd6
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2021-05-08 00:36:06 -0300

    Refactoring

--- src/main/java/com/thiagolvlsantos/gitt/storage/IGitStorage.java
+++ src/main/java/com/thiagolvlsantos/gitt/storage/IGitStorage.java
@@ -4,9 +4,17 @@ import java.io.File;
 
 public interface IGitStorage {
 
+	<T> boolean exists(File dir, Class<T> type, T reference);
+
+	<T> boolean exists(File dir, Class<T> type, Object... keys);
+
 	<T> T write(File dir, Class<T> type, T instance);
 
+	<T> T read(File dir, Class<T> type, T reference);
+
 	<T> T read(File dir, Class<T> type, Object... keys);
+
+	<T> T delete(File dir, Class<T> type, T instance);
 
 	<T> T delete(File dir, Class<T> type, Object... keys);
 }
--- src/main/java/com/thiagolvlsantos/gitt/storage/impl/GitStoreImpl.java
+++ src/main/java/com/thiagolvlsantos/gitt/storage/impl/GitStoreImpl.java
@@ -11,12 +11,15 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.thiagolvlsantos.gitt.storage.GitChanged;
 import com.thiagolvlsantos.gitt.storage.GitCreated;
 import com.thiagolvlsantos.gitt.storage.GitEntity;
@@ -39,162 +42,23 @@ public class GitStoreImpl implements IGitStorage {
 
 	private @Autowired ObjectMapper mapper;
 
+	@PostConstruct
+	public void configure() {
+		mapper = mapper.enable(SerializationFeature.INDENT_OUTPUT);
+	}
+
 	@Override
-	@SneakyThrows
-	public <T> T write(File dir, Class<T> type, T instance) {
-		// received fields
-		GitEntity entity = AnnotationUtils.findAnnotation(type, GitEntity.class);
-		if (log.isInfoEnabled()) {
-			log.info("entity: {}", entity);
-		}
-		PairValue<GitId>[] ids = get(GitId.class, type, instance);
-		if (log.isInfoEnabled()) {
-			log.info("ids: {}", Arrays.toString(ids));
-		}
+	public <T> boolean exists(File dir, Class<T> type, T reference) {
+		return exists(dir, type, keys(type, reference));
+	}
+
+	private <T> Object[] keys(Class<T> type, T instance) {
 		PairValue<GitKey>[] keys = get(GitKey.class, type, instance);
 		Arrays.sort(keys, (a, b) -> a.annotation.order() - b.annotation.order());
 		if (log.isInfoEnabled()) {
 			log.info("keys: {}", Arrays.toString(keys));
 		}
-		// updateable fields
-		PairValue<GitRevision>[] revisions = get(GitRevision.class, type, instance);
-		if (log.isInfoEnabled()) {
-			log.info("revisions: {}", Arrays.toString(revisions));
-		}
-		PairValue<GitCreated>[] created = get(GitCreated.class, type, instance);
-		if (log.isInfoEnabled()) {
-			log.info("created: {}", Arrays.toString(created));
-		}
-		PairValue<GitChanged>[] changed = get(GitChanged.class, type, instance);
-		if (log.isInfoEnabled()) {
-			log.info("changed: {}", Arrays.toString(changed));
-		}
-
-		File root = root(dir, entity);
-		File fileEntity = entity(root, keys);
-		File metadata = new File(fileEntity, "meta.json");
-		T old = null;
-		try {
-			old = read(dir, type, Stream.of(keys).map(k -> k.value).toArray());
-		} catch (Throwable e) {
-			// silent
-		}
-		prepareCreated(instance, created, metadata, old);
-		prepareRevisions(instance, revisions, metadata, old);
-		prepareChanged(instance, changed, metadata, old);
-		mapper.writeValue(metadata, instance);
-		return instance;
-	}
-
-	private <T> void prepareCreated(T instance, PairValue<GitCreated>[] created, File metadata, T old)
-			throws Exception {
-		if (!metadata.exists()) {
-			metadata.getParentFile().mkdirs();
-			for (PairValue<GitCreated> c : created) {
-				Object obj = c.getRead().invoke(instance);
-				if (obj == null) {
-					c.getWrite().invoke(instance, LocalDateTime.now());
-					if (log.isInfoEnabled()) {
-						log.info("new created: {}", c.getRead().invoke(instance));
-					}
-				}
-			}
-		} else {
-			for (PairValue<GitCreated> c : created) {
-				Object obj = c.getRead().invoke(old);
-				c.getWrite().invoke(instance, obj);
-				if (log.isInfoEnabled()) {
-					log.info("keep created: {}", c.getRead().invoke(instance));
-				}
-			}
-		}
-	}
-
-	private <T> void prepareRevisions(T instance, PairValue<GitRevision>[] revisions, File metadata, T old)
-			throws Exception {
-		for (PairValue<GitRevision> c : revisions) {
-			Number obj = (Number) c.getRead().invoke(instance);
-			if (obj == null) {
-				c.getWrite().invoke(instance, 0);
-			} else {
-				Number current = (Number) c.getRead().invoke(old);
-				c.getWrite().invoke(instance, current.longValue() + 1);
-			}
-			if (log.isInfoEnabled()) {
-				log.info("new revision: {}", c.getRead().invoke(instance));
-			}
-		}
-	}
-
-	private <T> void prepareChanged(T instance, PairValue<GitChanged>[] changed, File metadata, T old)
-			throws Exception {
-		for (PairValue<GitChanged> c : changed) {
-			c.getWrite().invoke(instance, LocalDateTime.now());
-			if (log.isInfoEnabled()) {
-				log.info("new changed: {}", c.getRead().invoke(instance));
-			}
-		}
-	}
-
-	private File root(File dir, GitEntity entity) {
-		return new File(dir, entity.value());
-	}
-
-	private File entity(File root, PairValue<GitKey>[] keys) {
-		File tmp = root;
-		for (PairValue<GitKey> k : keys) {
-			tmp = new File(tmp, String.valueOf(k.getValue()));
-		}
-		if (log.isInfoEnabled()) {
-			log.info("pack: {}", tmp);
-		}
-		return tmp;
-	}
-
-	@Override
-	@SneakyThrows
-	public <T> T read(File dir, Class<T> type, Object... keys) {
-		GitEntity entity = AnnotationUtils.findAnnotation(type, GitEntity.class);
-		if (log.isInfoEnabled()) {
-			log.info("entity: {}", entity);
-		}
-		File root = root(dir, entity);
-		File fileEntity = entity(root, keys);
-		File metadata = new File(fileEntity, "meta.json");
-		return mapper.readValue(metadata, type);
-	}
-
-	private File entity(File root, Object[] keys) {
-		File tmp = root;
-		for (Object k : keys) {
-			tmp = new File(tmp, String.valueOf(k));
-		}
-		if (log.isInfoEnabled()) {
-			log.info("pack: {}", tmp);
-		}
-		return tmp;
-	}
-
-	@Override
-	@SneakyThrows
-	public <T> T delete(File dir, Class<T> type, Object... keys) {
-		GitEntity entity = AnnotationUtils.findAnnotation(type, GitEntity.class);
-		if (log.isInfoEnabled()) {
-			log.info("entity: {}", entity);
-		}
-		File root = root(dir, entity);
-		File fileEntity = entity(root, keys);
-		T old = null;
-		if (fileEntity.exists()) {
-			old = read(dir, type, keys);
-			if (!FileUtils.delete(fileEntity.getParentFile())) {
-				throw new RuntimeException("Project not deleted. File:" + fileEntity);
-			} else {
-				Thread.sleep(100);
-			}
-
-		}
-		return old;
+		return Stream.of(keys).map(v -> v.getValue()).toArray();
 	}
 
 	@SuppressWarnings("unchecked")
@@ -223,17 +87,166 @@ public class GitStoreImpl implements IGitStorage {
 		return result.toArray(new PairValue[0]);
 	}
 
+	@Override
+	public <T> boolean exists(File dir, Class<T> type, Object... keys) {
+		return entityDir(dir, type, keys).exists();
+	}
+
+	private <T> File entityDir(File dir, Class<T> type, Object... keys) {
+		GitEntity entity = AnnotationUtils.findAnnotation(type, GitEntity.class);
+		if (log.isInfoEnabled()) {
+			log.info("entity: {}", entity);
+		}
+		File path = new File(dir, entity.value());
+		for (Object k : keys) {
+			path = new File(path, String.valueOf(k));
+		}
+		if (log.isInfoEnabled()) {
+			log.info("path: {}", path);
+		}
+		return path;
+	}
+
+	@Override
+	@SneakyThrows
+	public <T> T write(File dir, Class<T> type, T instance) {
+		File file = entityFile(dir, type, keys(type, instance));
+		T old = null;
+		if (file.exists()) {
+			old = read(file, type);
+		}
+		prepareCreated(type, instance, file, old);
+		prepareRevisions(type, instance, file, old);
+		prepareChanged(type, instance, file, old);
+
+		write(instance, file);
+
+		return instance;
+	}
+
+	private <T> void write(T instance, File file) throws Exception {
+		mapper.writeValue(file, instance);
+	}
+
+	private <T> File entityFile(File dir, Class<T> type, Object... keys) {
+		return new File(entityDir(dir, type, keys), "meta.json");
+	}
+
+	private <T> void prepareCreated(Class<T> type, T instance, File target, T old) throws Exception {
+		PairValue<GitId>[] ids = get(GitId.class, type, instance);
+		if (log.isInfoEnabled()) {
+			log.info("ids: {}", Arrays.toString(ids));
+		}
+
+		PairValue<GitCreated>[] created = get(GitCreated.class, type, instance);
+		if (log.isInfoEnabled()) {
+			log.info("created: {}", Arrays.toString(created));
+		}
+		if (!target.exists()) {
+			target.getParentFile().mkdirs();
+			for (PairValue<GitCreated> c : created) {
+				Object obj = c.getRead().invoke(instance);
+				if (obj == null) {
+					c.getWrite().invoke(instance, LocalDateTime.now());
+					if (log.isInfoEnabled()) {
+						log.info("new created: {}", c.getRead().invoke(instance));
+					}
+				}
+			}
+		} else {
+			for (PairValue<GitCreated> c : created) {
+				Object obj = c.getRead().invoke(old);
+				c.getWrite().invoke(instance, obj);
+				if (log.isInfoEnabled()) {
+					log.info("keep created: {}", c.getRead().invoke(instance));
+				}
+			}
+		}
+	}
+
+	private <T> void prepareRevisions(Class<T> type, T instance, File target, T old) throws Exception {
+		PairValue<GitRevision>[] revisions = get(GitRevision.class, type, instance);
+		if (log.isInfoEnabled()) {
+			log.info("revisions: {}", Arrays.toString(revisions));
+		}
+		for (PairValue<GitRevision> c : revisions) {
+			Number obj = (Number) c.getRead().invoke(instance);
+			if (obj == null) {
+				c.getWrite().invoke(instance, 0);
+			} else {
+				Number current = null;
+				if (old != null) {
+					current = (Number) c.getRead().invoke(old);
+				} else {
+					current = 0L;
+				}
+				c.getWrite().invoke(instance, current.longValue() + 1);
+			}
+			if (log.isInfoEnabled()) {
+				log.info("new revision: {}", c.getRead().invoke(instance));
+			}
+		}
+	}
+
+	private <T> void prepareChanged(Class<T> type, T instance, File target, T old) throws Exception {
+		PairValue<GitChanged>[] changed = get(GitChanged.class, type, instance);
+		if (log.isInfoEnabled()) {
+			log.info("changed: {}", Arrays.toString(changed));
+		}
+		for (PairValue<GitChanged> c : changed) {
+			c.getWrite().invoke(instance, LocalDateTime.now());
+			if (log.isInfoEnabled()) {
+				log.info("new changed: {}", c.getRead().invoke(instance));
+			}
+		}
+	}
+
+	@Override
+	public <T> T read(File dir, Class<T> type, T reference) {
+		return read(dir, type, keys(type, reference));
+	}
+
+	@Override
+	public <T> T read(File dir, Class<T> type, Object... keys) {
+		return read(entityFile(dir, type, keys), type);
+	}
+
+	@SneakyThrows
+	public <T> T read(File file, Class<T> type) {
+		return mapper.readValue(file, type);
+	}
+
+	@Override
+	public <T> T delete(File dir, Class<T> type, T reference) {
+		return delete(dir, type, keys(type, reference));
+	}
+
+	@Override
+	public <T> T delete(File dir, Class<T> type, Object... keys) {
+		File file = entityFile(dir, type, keys);
+		T old = null;
+		if (file.exists()) {
+			old = read(dir, type, keys);
+			if (!FileUtils.delete(file.getParentFile())) {
+				throw new RuntimeException("Project not deleted. File:" + file);
+			}
+		}
+		return old;
+	}
+
 	@Data
 	@AllArgsConstructor
 	@NoArgsConstructor
 	@Builder
-	@ToString
+	@ToString(onlyExplicitlyIncluded = true)
 	private static class PairValue<T> {
 		private T annotation;
 		private Field field;
 		private Method read;
 		private Method write;
+		@ToString.Include
 		private String name;
+		@ToString.Include
 		private Object value;
 	}
 }
