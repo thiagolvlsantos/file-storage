1ebd28cf193f71f6435555aa792672aff3c36159
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2021-05-10 00:57:31 -0300

    Support for ids generator.

--- src/main/java/com/thiagolvlsantos/git/storage/EnableGitStorage.java
+++ src/main/java/com/thiagolvlsantos/git/storage/EnableGitStorage.java
@@ -1,6 +1,8 @@
 package com.thiagolvlsantos.git.storage;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -10,5 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.thiagolvlsantos.git.storage")
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
 public @interface EnableGitStorage {
 }
--- src/main/java/com/thiagolvlsantos/git/storage/impl/GitStoreImpl.java
+++ src/main/java/com/thiagolvlsantos/git/storage/impl/GitStoreImpl.java
@@ -5,10 +5,13 @@ import java.io.File;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
@@ -113,11 +116,7 @@ public class GitStoreImpl implements IGitStorage {
 	}
 
 	private <T> File entityDir(File dir, Class<T> type, Object... keys) {
-		GitEntity entity = AnnotationUtils.findAnnotation(type, GitEntity.class);
-		if (log.isInfoEnabled()) {
-			log.info("entity: {}", entity);
-		}
-		File path = new File(dir, entity.value());
+		File path = entityRoot(dir, type);
 		for (Object k : keys) {
 			path = new File(path, String.valueOf(k));
 		}
@@ -125,6 +124,14 @@ public class GitStoreImpl implements IGitStorage {
 			log.info("path: {}", path);
 		}
 		return path;
+	}
+
+	private <T> File entityRoot(File dir, Class<T> type) {
+		GitEntity entity = AnnotationUtils.findAnnotation(type, GitEntity.class);
+		if (log.isInfoEnabled()) {
+			log.info("entity: {}", entity);
+		}
+		return new File(dir, entity.value());
 	}
 
 	@Override
@@ -135,9 +142,10 @@ public class GitStoreImpl implements IGitStorage {
 		if (file.exists()) {
 			old = read(file, type);
 		}
-		prepareCreated(type, instance, file, old);
-		prepareRevisions(type, instance, file, old);
-		prepareChanged(type, instance, file, old);
+
+		prepareCreated(dir, type, instance, file, old);
+		prepareRevisions(dir, type, instance, file, old);
+		prepareChanged(dir, type, instance, file, old);
 
 		write(instance, file);
 
@@ -152,24 +160,37 @@ public class GitStoreImpl implements IGitStorage {
 		return new File(entityDir(dir, type, keys), "meta.json");
 	}
 
-	private <T> void prepareCreated(Class<T> type, T instance, File target, T old) throws Exception {
+	private <T> void prepareCreated(File dir, Class<T> type, T instance, File target, T old) throws Exception {
 		PairValue<GitId>[] ids = get(GitId.class, type, instance);
 		if (log.isInfoEnabled()) {
 			log.info("ids: {}", Arrays.toString(ids));
 		}
-
 		PairValue<GitCreated>[] created = get(GitCreated.class, type, instance);
 		if (log.isInfoEnabled()) {
 			log.info("created: {}", Arrays.toString(created));
 		}
 		if (!target.exists()) {
-			target.getParentFile().mkdirs();
+			File parent = target.getParentFile();
+			if (!parent.mkdirs()) {
+				throw new RuntimeException("Could not create object directory: " + parent);
+			}
 			for (PairValue<GitCreated> c : created) {
 				Object obj = c.getRead().invoke(instance);
 				if (obj == null) {
 					c.getWrite().invoke(instance, LocalDateTime.now());
 					if (log.isInfoEnabled()) {
 						log.info("new created: {}", c.getRead().invoke(instance));
+					}
+				}
+			}
+			for (PairValue<GitId> c : ids) {
+				Object obj = c.getRead().invoke(instance);
+				if (obj == null) {
+					Long nextId = generateId(dir, type);
+					generateIndex(dir, type, instance, nextId);
+					c.getWrite().invoke(instance, nextId);
+					if (log.isInfoEnabled()) {
+						log.info("new id: {}", c.getRead().invoke(instance));
 					}
 				}
 			}
@@ -181,10 +202,62 @@ public class GitStoreImpl implements IGitStorage {
 					log.info("keep created: {}", c.getRead().invoke(instance));
 				}
 			}
+			for (PairValue<GitId> c : ids) {
+				Object obj = c.getRead().invoke(old);
+				c.getWrite().invoke(instance, obj);
+				if (log.isInfoEnabled()) {
+					log.info("keep ids: {}", c.getRead().invoke(instance));
+				}
+			}
 		}
 	}
 
-	private <T> void prepareRevisions(Class<T> type, T instance, File target, T old) throws Exception {
+	@SneakyThrows
+	private <T> Long generateId(File dir, Class<T> type) {
+		Long next = currentId(dir, type) + 1;
+		Files.write(currentIdFile(dir, type).toPath(), String.valueOf(next).getBytes(), StandardOpenOption.CREATE,
+				StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+		return next;
+	}
+
+	@SneakyThrows
+	private <T> Long currentId(File dir, Class<T> type) {
+		Long next = 0L;
+		File current = currentIdFile(dir, type);
+		if (current.exists()) {
+			next = Long.valueOf(Files.readString(current.toPath()));
+		}
+		return next;
+	}
+
+	private <T> File currentIdFile(File dir, Class<T> type) {
+		return new File(entityRoot(dir, type), ".current");
+	}
+
+	@SneakyThrows
+	private <T> void generateIndex(File dir, Class<T> type, T instance, Long nextId) {
+		File index = directoryIndex(dir, type);
+		if (!index.exists() && !index.mkdirs()) {
+			throw new RuntimeException("Could not create index directory: " + index);
+		}
+		File fileRef = fileRef(dir, type, nextId);
+		Object[] keys = keys(type, instance);
+		Files.write(fileRef.toPath(),
+				Stream.of(keys).map(k -> String.valueOf(k)).collect(Collectors.joining("\n")).getBytes(),
+				StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+	}
+
+	private <T> File directoryIndex(File dir, Class<T> type) {
+		return new File(entityRoot(dir, type), ".all");
+	}
+
+	private <T> File fileRef(File dir, Class<T> type, Object... ids) {
+		return new File(directoryIndex(dir, type),
+				Stream.of(ids).map(k -> String.valueOf(k)).collect(Collectors.joining("_")));
+	}
+
+	@SneakyThrows
+	private <T> void prepareRevisions(File dir, Class<T> type, T instance, File target, T old) {
 		PairValue<GitRevision>[] revisions = get(GitRevision.class, type, instance);
 		if (log.isInfoEnabled()) {
 			log.info("revisions: {}", Arrays.toString(revisions));
@@ -211,15 +284,16 @@ public class GitStoreImpl implements IGitStorage {
 		}
 	}
 
-	private <T> void prepareChanged(Class<T> type, T instance, File target, T old) throws Exception {
+	private <T> void prepareChanged(File dir, Class<T> type, T instance, File target, T old) throws Exception {
 		PairValue<GitChanged>[] changed = get(GitChanged.class, type, instance);
 		if (log.isInfoEnabled()) {
 			log.info("changed: {}", Arrays.toString(changed));
 		}
 		for (PairValue<GitChanged> c : changed) {
-			c.getWrite().invoke(instance, LocalDateTime.now());
+			Method read = c.getRead();
+			c.getWrite().invoke(instance, read.getReturnType().getMethod("now").invoke(null));
 			if (log.isInfoEnabled()) {
-				log.info("new changed: {}", c.getRead().invoke(instance));
+				log.info("new changed: {}", read.invoke(instance));
 			}
 		}
 	}
@@ -251,10 +325,23 @@ public class GitStoreImpl implements IGitStorage {
 		if (file.exists()) {
 			old = read(dir, type, keys);
 			if (!FileUtils.delete(file.getParentFile())) {
-				throw new RuntimeException("Project not deleted. File:" + file);
+				throw new RuntimeException("Entity not deleted. File:" + file);
+			}
+			File ref = fileRef(dir, type, ids(type, old));
+			if (ref.exists() && !ref.delete()) {
+				throw new RuntimeException("Could no delete entity index: " + file);
 			}
 		}
 		return old;
+	}
+
+	private Object[] ids(Class<?> type, Object instance) {
+		PairValue<GitId>[] ids = get(GitId.class, type, instance);
+		Object[] result = Stream.of(ids).map(v -> v.getValue()).toArray();
+		if (log.isInfoEnabled()) {
+			log.info("ids: {}", Arrays.toString(result));
+		}
+		return result;
 	}
 
 	@Data
